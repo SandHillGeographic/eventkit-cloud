a10363af4af678819773397807698c4c0a6bc834
root <root@ip-172-30-1-207.ec2.internal>
2019-01-02 19:59:49 +0000

    added ssl

--- scripts/eventkit-start.sh
+++ scripts/eventkit-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 chown -R eventkit:eventkit /var/log/eventkit /var/lib/eventkit
-cd /var/lib/eventkit && gunicorn eventkit_cloud.wsgi:application --bind 0.0.0.0:6080 --worker-class eventlet --workers 1 --threads 2 --name eventkit --user eventkit --no-sendfile --reload
+cd /var/lib/eventkit && gunicorn eventkit_cloud.wsgi:application --bind 0.0.0.0:6443 --certfile=certificate.pem --keyfile=key.pem --worker-class eventlet --workers 1 --threads 2 --name eventkit --user eventkit --no-sendfile --reload
